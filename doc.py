3289506addbb1dac586ed693b590ce3d37996a7e
junior-delatorre <43044032+junior-delatorre@users.noreply.github.com>
2018-09-07 09:22:50 -0700

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,7 @@
+# doc.py jd 
+""" document how 
+	you write your 
+	code.
+"""
+print (" hello world ")
+print (" document your work ")
---
+++ vars.py
@@ -0,0 +1,7 @@
+#vars.py
+pi = 3.1415626535
+e = 2.7182818284590
+c = "COLEMAN"
+PRODUCT = e ** pi
+print (product) 
+print(c,pi,e)
